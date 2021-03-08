0c99f08da7b9967a86e7a337f5bf3795ba650f69
Enzo Taurines <enzodylan.taurines@gmail.com>
2021-03-08 10:00:09 +0100

    Pour le plaisir

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('infos', [\App\Http\Controllers\ApiBookingController::class, 'getBooking']);
 
-Route::post('booking', [ \App\Http\Controllers\ApiBookingController::class, 'addBooking']);
+Route::post('reservation', [ \App\Http\Controllers\ApiBookingController::class, 'addBooking']);
 
-Route::post('booking/{token}', [\App\Http\Controllers\ApiBookingController::class, 'deleteBooking']);
+Route::post('reservation/annulation/{token}', [\App\Http\Controllers\ApiBookingController::class, 'deleteBooking']);
 
